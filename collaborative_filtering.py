a0525e96e50cd5326f52dd5c1e6dbd8fbdd04e98
Yang LI <zjzsliyang@gmail.com>
2017-10-03 15:40:53 +0800

    add data processing part

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -1,49 +1,73 @@
 import csv
+import heapq
+from numpy import *
 
-class Data(object):
-    """shopping data.
+def read_data(month):
+    user_list = []
+    item_list = []
+    with open('data/{0}.csv'.format(month)) as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            user_id = row[0]
+            item_id = row[1]
+            count = int(row[2])
+            user_exist = False
+            item_exist = False
+            for item in item_list:
+                if item_id == item:
+                    item_exist = True
+            if not item_exist:
+                item_list.append(item_id)
+            for user in user_list:
+                if user_id == user.user_id:
+                    user_exist = True
+                    user.item_rank[item_id] = count + user.item_rank.get(item_id, 0)
+            if not user_exist:
+                user = User(user_id, {item_id: count})
+                user_list.append(user)
+    return (user_list, item_list)
 
-    Args:
-        user_id: user id.
-        item_id: item id.
-        count: count of user purchases.
-        qty: total number of item.
-        amt: total sum up.
-    """
+def cosine_similarity(user_list, item_list):
+    item_len = len(item_list)
+    item_matrix = mat(zeros((item_len, item_len)))
+    for user in user_list:
+        for item_1 in user.item_rank:
+            for item_2 in user.item_rank:
+                if item_1 != item_2:
+                    item_matrix[item_list.index(item_1), item_list.index(item_2)] += 1
 
-    def __init__(self, month):
-        self.user_id = []
-        self.item_id = []
-        self.count = []
-        self.qty = []
-        self.amt = []
-        with open('data/{0}.csv'.format(month)) as csv_file:
-            reader = csv.reader(csv_file)
-            for row in reader:
-                self.user_id.append(row[0])
-                self.item_id.append(row[1])
-                self.count.append(row[2])
-                self.qty.append(row[3])
-                self.amt.append(row[4])
+    for i in range(item_len):
+        sum_column = sum(item_matrix, axis=1)[i, 0]
+        for j in range(item_len):
+            sum_row = sum(item_matrix, axis=0)[0, j]
+            times = sum_column * sum_row
+            if times != 0:
+                item_matrix[i, j] = item_matrix[i, j] / (sqrt(times))
+    return item_matrix
 
-    def euclidean_distance(self):
-        pass
+def top_n(item_matrix, n=3):
+    recommendation_list = [[0] * n] * item_matrix.shape[0]
+    for i in range(item_matrix.shape[0]):
+        recommendation_list[i] = heapq.nlargest(n, item_matrix.tolist()[i])
+    return recommendation_list
+    
+def precision():
+    pass
 
-    def cosine_similarity(self, parameter_list):
-        pass
+def recall():
+    pass
 
-    def k_neighborhoods(self):
-        pass
-
-    def test(self):
-        pass
+class User(object):
+    def __init__(self, user_id, item_rank):
+        self.user_id = user_id
+        self.item_rank = item_rank
 
 def main():
-    """item-based collaborative filtering
-    """
+    ''' item-based collaborative filtering'''
 
-    training_data = Data('july')
-
+    user_list, item_list = read_data('test')
+    item_matrix = cosine_similarity(user_list, item_list)
+    recommendation_list = top_n(item_matrix)
 
 if __name__ == '__main__':
     main()
