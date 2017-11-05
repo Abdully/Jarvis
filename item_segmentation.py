7d2dd406519f6e220d07c86f62178f8bb094ed93
Yang LI <zjzsliyang@gmail.com>
2017-11-05 12:33:43 +0800

    Add thulac support

--- item_segmentation.py
+++ item_segmentation.py
@@ -2,6 +2,7 @@ import csv
 import time
 import jieba
 import timeit
+import thulac
 import logging
 
 def read_data(month):
@@ -15,6 +16,7 @@ def read_data(month):
             pluname = row[9]
             spec = row[10]
             pkunit = row[11]
+            plutype = row[13]
             bndname = row[15]
             bcd_exist = False
             for item in bcd_list:
@@ -26,17 +28,28 @@ def read_data(month):
     logging.info('complete loading data in {0}.'.format(month))
     return (bcd_list, pluname_list)
 
+def jieba_(pluname_list):
+    for pluname in pluname_list:
+        seg_pluname = jieba.cut_for_search(pluname)
+        print("/ ".join(seg_pluname))
+
+def thulac_(pluname_list):
+    thu = thulac.thulac(seg_only=True)
+    for pluname in pluname_list:
+        print(thu.cut(pluname, text=True))
+
+
 def main():
     logging.basicConfig(filename='cf.log', level=logging.INFO)
     logging.info(time.asctime(time.localtime(time.time())))
     start_time = timeit.default_timer()
 
-    # jieba.enable_parallel()
-
     _, pluname_list = read_data('2016-07-01_2016-07-31')
-    for pluname in pluname_list:
-        seg_pluname = jieba.cut(pluname, cut_all=False)
-        print("/ ".join(seg_pluname))
+    
+    # jieba.enable_parallel()
+    # jieba_(pluname_list)
+    
+    thulac_(pluname_list)
 
     stop_time = timeit.default_timer()
     logging.info('run in {0} seconds.\n'.format(stop_time - start_time))
