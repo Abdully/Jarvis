94bcab9fc472ee999584aa18c3dfc00bf2950e67
Yang LI <zjzsliyang@gmail.com>
2017-12-19 11:06:42 +0800

    fix bugs

--- frequent_pattern/verification.py
+++ frequent_pattern/verification.py
@@ -9,90 +9,101 @@ def main():
     middle = datetime(year=2016, month=12, day=1)
     end = datetime(year=2016, month=12, day=31)
     window = timedelta(days=14)
-    top = 10
+    top_begin = 5
+    top_end = 15
 
-    reco = {}
-    with open('output_data_viponly_{}_{}_{}'.format(d.date(), end.date(), window.days), 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            # print(line)
-            cnt = 0
-            base_items = []
-            for content in line.split():
-                if content == '-1':
-                    cnt += 1
-                elif content == '#SUP:':
-                    break
-                else:
-                    if cnt == 0:
-                        if content not in base_items:
-                            base_items.append(content)
-                        if content not in reco:
-                            reco[content] = {}                            
+    precision = []
+    recall = []
+    for top in range(top_begin, top_end, 1):
+        reco = {}
+        with open('output_data_viponly_{}_{}_{}'.format(d.date(), end.date(), window.days), 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                # print(line)
+                cnt = 0
+                base_items = []
+                for content in line.split():
+                    if content == '-1':
+                        cnt += 1
+                    elif content == '#SUP:':
+                        break
                     else:
-                        # print('---')
-                        # print(line)
-                        # print(content)
-                        # print(base_items)
-                        for item in base_items:
-                            if line.split()[-1] not in reco[item].keys():
-                                reco[item][line.split()[-1]] = []
-                            if content not in reco[item][line.split()[-1]]:
-                                reco[item][line.split()[-1]] = [content]
+                        if cnt == 0:
+                            if content not in base_items:
+                                base_items.append(content)
+                            if content not in reco:
+                                reco[content] = {}                            
+                        else:
+                            # print('---')
+                            # print(line)
+                            # print(content)
+                            # print(base_items)
+                            for item in base_items:
+                                if line.split()[-1] not in reco[item].keys():
+                                    reco[item][line.split()[-1]] = []
+                                if content not in reco[item][line.split()[-1]]:
+                                    reco[item][line.split()[-1]] = [content]
 
-    for item in list(reco):
-        if reco[item] == {}:
-            del(reco[item])
+        for item in list(reco):
+            if reco[item] == {}:
+                del(reco[item])
 
-    reco2 = {}
-    for item in reco.keys():
-        reco2[item] = {}
-        for grade in sorted(reco[item].keys(), reverse=True)[0:top]:
-            reco2[item][grade] = reco[item][grade]
+        reco2 = {}
+        for item in reco.keys():
+            reco2[item] = {}
+            for grade in sorted(reco[item].keys(), reverse=True)[0:top]:
+                reco2[item][grade] = reco[item][grade]
 
-    # for item in reco.keys():
-    #     print(reco[item])
+        # for item in reco.keys():
+        #     print(reco[item])
 
-    connection = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgres123456'")
-    cur = connection.cursor()
-    cur.execute("""SELECT uid, pluno FROM original_transaction
-                      WHERE sldat::DATE >= DATE %s AND sldat::DATE <= %s""",
-                 (begin, middle))
-    transactions = cur.fetchall()
+        connection = psycopg2.connect("dbname='postgres' user='postgres' host='localhost' password='postgres123456'")
+        cur = connection.cursor()
+        cur.execute("""SELECT uid, pluno FROM original_transaction
+                            WHERE sldat::DATE >= DATE %s AND sldat::DATE <= %s""",
+                        (begin, middle))
+        transactions = cur.fetchall()
 
-    users = {}
-    for transaction in transactions:
-        if transaction[1] in reco.keys():
-            if transaction[0] not in users.keys():
-                users[transaction[0]] = {}
-            for grade, content in reco[transaction[1]].items():
-                users[transaction[0]][grade] = content
+        users = {}
+        for transaction in transactions:
+            if transaction[1] in reco.keys():
+                if transaction[0] not in users.keys():
+                    users[transaction[0]] = {}
+                for grade, content in reco[transaction[1]].items():
+                    users[transaction[0]][grade] = content
 
-    users2 = {}
-    for user in users.keys():
-        users2[user] = {}
-        for grade in sorted(users[user].keys(), reverse=True)[0:top]:
-            users2[user][grade] = users[user][grade]
+        users2 = {}
+        for user in users.keys():
+            users2[user] = {}
+            for grade in sorted(users[user].keys(), reverse=True)[0:top]:
+                users2[user][grade] = users[user][grade]
 
-    cur.execute("""SELECT uid, pluno FROM original_transaction
-                      WHERE sldat::DATE >= DATE %s AND sldat::DATE <= %s""",
-                 (middle, end))
-    verifications = cur.fetchall()
-    hit = 0
-    precisiontotal = 0
-    recalltotal = 0
-    for verification in verifications:
-        if verification[0] in users2.keys():
-            for samesup in users2[verification[0]].values():
-                if verification[1] in samesup:
-                    hit += 1
-                precisiontotal += 1
+        cur.execute("""SELECT uid, pluno FROM original_transaction
+                            WHERE sldat::DATE >= DATE %s AND sldat::DATE <= %s""",
+                        (middle, end))
+        verifications = cur.fetchall()
+        hit = 0
+        precisiontotal = 0
+        recalltotal = 0
+        for verification in verifications:
+            if verification[0] in users2.keys():
+                for samesup in users2[verification[0]].values():
+                    if verification[1] in samesup:
+                        hit += 1
+                    precisiontotal += 1
             recalltotal += 1
-    print('hit: {}'.format(hit))
-    print('precision total: {}'.format(precisiontotal))
-    print('recall total: {}'.format(recalltotal))
-    print('precision: {}'.format(hit/precisiontotal))
-    print('recall: {}'.format(hit/recalltotal))
+        print('top n: {}'.format(top))
+        print('hit: {}'.format(hit))
+        print('precision total: {}'.format(precisiontotal))
+        print('recall total: {}'.format(recalltotal))
+        print('precision: {}'.format(hit/precisiontotal))
+        print('recall: {}'.format(hit/recalltotal))
+        print('-----------------------')
+        precision.append(hit/precisiontotal)
+        recall.append(hit/recalltotal)
+
+    print(precision)
+    print(recall)
 
 if __name__ == '__main__':
     main()
